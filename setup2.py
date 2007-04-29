b47515f8817cac807d02354dc6a9ee45882c92fd
rjmarsan <rjmarsan@eb88d6b4-1ab2-431c-8348-e75b24139d5b>
2007-04-29 07:05:44 +0000

    added the py2exe script that doesnt work currently. hey never know

---
+++ setup2.py
@@ -0,0 +1,21 @@
+#this is my attempt at a py2exe script, it worked before but now its dead...
+from distutils.core import setup
+import py2exe, pygame
+import glob, shutil
+
+setup(windows=["pylaga.py"],
+name='Pylaga',
+version='0.0.14',
+description='A small Galaga clone',
+author='RJ Marsan',
+author_email='RJMarsan at gmail.com',
+url='http://rj.selfip.com',
+py_modules=['bullet','background','display','enemy','globalvars','player','stage','menu','gun','ecollide','menu','menulists','token','AHH','stages','enemys','guns','bullets']
+)
+
+shutil.copytree('data', 'dist/data')
+shutil.copytree('enemys', 'dist/enemys')
+shutil.copytree('bullets', 'dist/bullets')
+shutil.copytree('guns', 'dist/guns')
+shutil.copytree('stages', 'dist/stages')
+shutil.copyfile('freesansbold.ttf', 'dist/freesansbold.ttf')

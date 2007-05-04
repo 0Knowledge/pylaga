25522e5fa35267984be704e2a9d826910ce0b032
rjmarsan <rjmarsan@eb88d6b4-1ab2-431c-8348-e75b24139d5b>
2007-05-04 19:37:11 +0000

    Just fixed a small bug so the ships return to normal after the game resets

--- stages/stage1.py
+++ stages/stage1.py
@@ -4,3 +4,9 @@ def start(p):
 	p.enemyodds=100
 	for player in p.playermanager:
 		player.change_gun("Gun","Bullet",p.playerbulletmanager,2)
+	from enemy import Enemy
+	from gun import EnemyGun
+	from bullet import EnemyBullet
+	p.enemyclass=Enemy
+	p.enemygunclass=EnemyGun
+	p.enemybulletclass=EnemyBullet

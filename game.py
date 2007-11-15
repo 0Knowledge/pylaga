25900ed6423e68bc59e9b779f7312ea07be58945
rjmarsan <rjmarsan@eb88d6b4-1ab2-431c-8348-e75b24139d5b>
2007-11-15 18:38:46 +0000

    hoh man. i made this fscker gpl3. woot woot wut woot.

--- game.py
+++ game.py
@@ -2,6 +2,26 @@
 #Pylaga
 #Original: 2007-02-20 Derek Mcdonald 
 #Subclass of pylaga.py
+
+#######################################################################
+#    This file is part of pylaga.
+#
+#    Pylaga is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    pylaga is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Pylaga.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    YAY FOR LONG LICENSES!!!! screw you society.
+#######################################################################
+
 #################################################################################################################
 #
 #
